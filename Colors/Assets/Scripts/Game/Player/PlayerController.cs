3e5727dc456a643a1c1d43c3db56da10716fc3ca
Danilock <yo1234re@gmail.com>
2020-12-18 10:44:07 -0400

    Initial Commit

---
+++ Colors/Assets/Scripts/Game/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        
+    }
+}
